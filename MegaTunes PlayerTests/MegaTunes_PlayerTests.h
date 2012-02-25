84c821532958b1f1cc3c8b4c43e6164cc4aec5d7
Lori Hill <frostbitelearning@gmail.com>
2012-02-24 19:30:12 -0800

    Initial Commit

---
+++ MegaTunes PlayerTests/MegaTunes_PlayerTests.h
@@ -0,0 +1,13 @@
+//
+//  MegaTunes_PlayerTests.h
+//  MegaTunes PlayerTests
+//
+//  Created by Lori Hill on 2/24/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface MegaTunes_PlayerTests : SenTestCase
+
+@end
---
+++ MegaTunes PlayerTests/MegaTunes_PlayerTests.m
@@ -0,0 +1,32 @@
+//
+//  MegaTunes_PlayerTests.m
+//  MegaTunes PlayerTests
+//
+//  Created by Lori Hill on 2/24/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "MegaTunes_PlayerTests.h"
+
+@implementation MegaTunes_PlayerTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in MegaTunes PlayerTests");
+}
+
+@end
